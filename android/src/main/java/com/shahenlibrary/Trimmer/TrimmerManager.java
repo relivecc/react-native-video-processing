e66ee5415db4fe16aef107f954aae8fa235eb719
Jasper Meijaard <jasper@relive.cc>
2020-05-29 14:27:15 +0200

    Revert "Update ffmpeg binaries to v4.3.1 and include 32+64-bit Android ABIs (#4)"
    This reverts commit 598bf25e73e848b551a1f59b4913084e0f2b24d0.

--- android/src/main/java/com/shahenlibrary/Trimmer/TrimmerManager.java
+++ android/src/main/java/com/shahenlibrary/Trimmer/TrimmerManager.java
@@ -124,5 +124,7 @@ public class TrimmerManager extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  private void loadFfmpeg() {}
+  private void loadFfmpeg() {
+    Trimmer.loadFfmpeg(reactContext);
+  }
 }
